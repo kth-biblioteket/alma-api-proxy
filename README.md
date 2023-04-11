f9d8c0d68dd15f5b647847828b93f3b013dd9a63
thompa21 <tholind@kth.se>
2023-04-11 18:32:29 +0200

    first

---
+++ README.md
@@ -0,0 +1,62 @@
+# KTHB Proxy mot Almas API
+
+##
+
+###
+
+
+#### Dependencies
+
+Node 16.13.2
+
+##### Installation
+
+1.  Skapa folder på server med namnet på repot: "/local/docker/alma-api-proxy"
+2.  Skapa och anpassa docker-compose.yml i foldern
+```
+version: '3.6'
+
+services:
+  ldap-api:
+    container_name: alma-api-proxy
+    image: ghcr.io/kth-biblioteket/alma-api-proxy:${REPO_TYPE}
+    restart: always
+    env_file:
+      - ./alma-api-proxy.env
+    labels:
+      - "traefik.enable=true"
+      - "traefik.http.routers.alma-api-proxy.rule=Host(`${DOMAIN_NAME}`) && PathPrefix(`${PATHPREFIX}`)"
+      - "traefik.http.routers.alma-api-proxy.middlewares=alma-api-proxy-stripprefix"
+      - "traefik.http.middlewares.alma-api-proxy-stripprefix.stripprefix.prefixes=${PATHPREFIX}"
+      - "traefik.http.routers.alma-api-proxy.entrypoints=websecure"
+      - "traefik.http.routers.alma-api-proxy.tls=true"
+      - "traefik.http.routers.alma-api-proxy.tls.certresolver=myresolver"
+    networks:
+      - "apps-net"
+
+networks:
+  apps-net:
+    external: true
+```
+3.  Skapa och anpassa .env(för composefilen) i foldern
+```
+PATHPREFIX=/alma
+DOMAIN_NAME=api-ref.lib.kth.se
+REPO_TYPE=ref
+```
+4.  Skapa och anpassa alma-api-proxy.env (för applikationen) i foldern
+```
+ALMAPIENDPOINT=https://api-eu.hosted.exlibrisgroup.com/almaws/v1/
+ALMAAPIKEY=xxxxxxxxxxxxxxxx
+PORT=3008
+EXLIBRISPUBLICKEY_X=xxxxxxxxxxxxxxxx
+EXLIBRISPUBLICKEY_Y=xxxxxxxxxxxxxxxx
+EXLIBRISPUBLICKEY_URL=https://api-eu.hosted.exlibrisgroup.com/auth/46KTH_INST/jwks.json?env=sandbox
+LOG_LEVEL=debug
+NODE_ENV=development
+```
+5. Skapa deploy_ref.yml i github actions
+6. Skapa deploy_prod.yml i github actions
+7. Github Actions bygger en dockerimage i github packages
+8. Starta applikationen med docker compose up -d --build i "local/docker/ldap-api"
+
---
+++ index.js
@@ -0,0 +1,189 @@
+
+require('dotenv').config({ path: 'alma-api-proxy.env'})
+const jwt = require('jsonwebtoken');
+const jwkToPem = require('jwk-to-pem');
+const axios = require('axios')
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require('cors')
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.use(cors({ origin: 'exlibrisgroup.com' }));
+
+const appRoutes = express.Router();
+
+
+
+appRoutes.get("/", async function (req, res, next) {
+    let adminuser = false
+    //Verifiera token från Primo
+    decodedtoken = verifytoken(req.query.jwt)
+    if (decodedtoken!=0) {
+        //Kontrollera användarens roller
+        try {
+            const almauser = await axios.get(process.env.ALMAPIENDPOINT + 'users/' + decodedtoken.userName + '?apikey=' + process.env.ALMAAPIKEY)
+            for (let index = 0; index < almauser.data.user_role.length; index++) {
+                const element = almauser.data.user_role[index].role_type.desc;
+                if(element.indexOf("User Manager") !== -1) {
+                    adminuser = true;
+                } else {
+                    res.status(400)
+                    res.json("Not authorized!");
+                }
+            }
+        } catch(err) {
+            res.status(400)
+            res.json(err.message);
+        }
+
+        try {
+            if(adminuser) {
+                const almaresponse = await axios.get(process.env.ALMAPIENDPOINT + req.query.apipath + '?apikey=' + process.env.ALMAAPIKEY)
+                res.json(almaresponse.data);
+            }
+        } catch(err) {
+            res.status(400)
+            res.json(err.message);
+        }
+    } else {
+        res.status(400)
+        res.json("None or not a valid token");
+    }
+})
+
+appRoutes.post("/activatepatron", async function (req, res, next) {
+    decodedtoken = verifytoken(req.query.jwt)
+    if (decodedtoken!=0) {
+        try {
+            //hämta user objekt
+            almapiurl = process.env.ALMAPIENDPOINT + 'users/' + decodedtoken.userName + '?apikey=' + process.env.ALMAAPIKEY
+            console.log(almapiurl)
+            const almauser = await axios.get(almapiurl)
+            
+            //Lägg till user note i hämtat userobjekt
+            almauser.data.user_note.push({
+                "note_type": {
+                    "value": "POPUP",
+                    "desc": "General"
+                },
+                "note_text": "Activated from Primo",
+                "segment_type": "Internal"
+            })
+            //Uppdatera patron rollen i hämtat userobjekt till att vara aktiv
+            let patronrole = false
+            for (let index = 0; index < almauser.data.user_role.length; index++) {
+                const element = almauser.data.user_role[index].role_type.desc;
+                if(element.indexOf("Patron") !== -1) {
+                    almauser.data.user_role[index].status.desc="Active"
+                    almauser.data.user_role[index].status.value="ACTIVE"
+                    result = "OK"
+                    patronrole = true
+                    break;
+                } else {
+                    result = "User does not have a patron role!";
+                }
+            }
+            if(patronrole) {
+                //Uppdatera alma
+                const almaresult = await axios.put(almapiurl, almauser.data)
+            }
+            res.json("success");
+        } catch(err) {
+            res.status(400)
+            res.json(err)
+        }
+    } else {
+        res.status(400)
+        res.json("None or not valid token")
+    }
+});
+
+appRoutes.put("/activatepatron", async function (req, res, next) {
+    decodedtoken = verifytoken(req.params.jwt)
+    if (decodedtoken!=0) {
+        try {  
+            almapiurl = process.env.ALMAPIENDPOINT + req.params.apipath + '&apikey=' + process.env.ALMAAPIKEY
+            const almaresponse = await axios.put(almapiurl, data, )
+            res.json(almaresponse);
+        } catch(err) {
+            res.status(400)
+            res.json(err.message)
+        }
+    } else {
+        res.status(400)
+        res.json("None or not valid token")
+    }
+});
+
+appRoutes.delete("/", async function (req, res, next) {
+
+    let adminuser = false
+    //Verifiera token från Primo
+    decodedtoken = verifytoken(req.query.jwt)
+    if (decodedtoken!=0) {
+        //Kontrollera användarens roller
+        try {
+            const almauser = await axios.get(process.env.ALMAPIENDPOINT + 'users/' + decodedtoken.userName + '?apikey=' + process.env.ALMAAPIKEY)
+            for (let index = 0; index < almauser.data.user_role.length; index++) {
+                const element = almauser.data.user_role[index].role_type.desc;
+                if(element.indexOf("User Manager") !== -1) {
+                    adminuser = true;
+                } else {
+                    res.status(400)
+                    res.json("Not authorized!");
+                }
+            }
+        } catch(err) {
+            res.status(400)
+            res.json(err.message);
+        }
+
+        try {
+            if(adminuser) {
+                const almaresponse = await axios.delete(process.env.ALMAPIENDPOINT + req.query.apipath + '?apikey=' + process.env.ALMAAPIKEY)
+                res.json(almaresponse);
+            }
+        } catch(err) {
+            res.status(400)
+            res.json(err.message);
+        }
+    } else {
+        res.status(400)
+        res.json("None or not a valid token");
+    }
+});
+
+app.use('/alma', appRoutes);
+
+const server = app.listen(process.env.PORT || 3002, function () {
+    const port = server.address().port;
+    console.log("App now running on port", port);
+});
+
+function verifytoken(tokenValue) {
+    //public key: https://api-eu.hosted.exlibrisgroup.com/auth/46KTH_INST/jwks.json
+    //public key: https://api-eu.hosted.exlibrisgroup.com/auth/46KTH_INST/jwks.json?env=sandbox
+    var keys = {
+        kty: "EC",
+        kid: "primaPrivateKey-46KTH_INST",
+        use: "sig",
+        x: process.env.EXLIBRISPUBLICKEY_X,
+        y: process.env.EXLIBRISPUBLICKEY_Y,
+        crv: "P-256",
+        alg: "ES256"
+    }
+
+    var pem = jwkToPem(keys);
+
+    try {
+        var token = jwt.verify( tokenValue, pem )
+        return token
+    } catch (err) {
+        return 0
+    }
+
+}
